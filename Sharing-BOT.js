271e4285b888cffd84dbcb04dec36645f6ecac71
whatdfc <44030799+whatdfc@users.noreply.github.com>
2018-10-11 09:53:53 +0200

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,19 @@
+﻿const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل السيرفر؟
+                              [ https://discord.gg/rn8wG5 ]
+                              [ https://discord.gg/vKEehB ]
+                              [ https://discord.gg/KgmCMM ]
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD.login("NDk0NjcxMDY5NTA0ODY0MjU2.Dp4PPw.4Ptv0gxnD-dB1DRwCbN3MluS3v8");
---
+++ Start.bat
@@ -0,0 +1,3 @@
+node Sharing-BOT
+
+
